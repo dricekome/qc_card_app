2724a2a691dad11d739ef2ca1f6a1483535d41eb
dricekome <jump.in.newworld.2000@gmail.com>
2025-05-14 16:55:57 +0900

    削除機能修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "custom/team_unique_select", to: "custom/team_unique_select.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get "card_draw/index"
-  get "pages/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  #root 'pages#index'
   root 'card_draw#index'
-  post 'quest_cards', to:'quest_cards#create'#csvのアップロード処理
+
   get "card_draw/index"
   post "card_draw/draw", to:"card_draw#draw"
-  get 'quest_cards/download',to: 'quest_cards#download'#csvダウンロード
-  resources :quest_cards
+  get 'quest_cards/download', to: 'quest_cards#download'
 
+  resources :quest_cards do
+    collection do
+      get :edit_multiple
+      patch :update_multiple
+    end
+  end
+
+  get "pages/index"
+  get "up" => "rails/health#show", as: :rails_health_check
 end
